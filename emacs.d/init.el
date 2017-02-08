8306c23b502ae744f2d06fa95eba356c3385472b
Jack Ransom <jackmransom@gmail.com>
2017-02-08 08:25:29 +0000

    Add custom colour highlighting to keywords in C comments in emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -58,3 +58,8 @@
 
 (setq ring-bell-function 'ignore)
 
+(add-hook 'c-mode-common-hook
+               (lambda ()
+                (font-lock-add-keywords nil
+                 '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))))
+
